5fd675d30c27c4b6ca34d63684b41e9f5a0bf070
arjunkotari <arjun.kotari123@gmail.com>
2021-07-12 19:15:00 +0530

    Comparison Computation

---
+++ src/LineComperison/Line_Compersion.java
@@ -0,0 +1,10 @@
+package LineComperison;
+
+import java.util.Scanner;
+
+public class Line_Compersion {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+
+}
