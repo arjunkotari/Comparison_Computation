04fa215cd48a23c0c985a834b61759005d911b0e
arjunkotari <arjun.kotari123@gmail.com>
2021-07-12 20:08:22 +0530

    Added: java compare to method

--- src/LineComperison/Line_Compersion.java
+++ src/LineComperison/Line_Compersion.java
@@ -30,17 +30,32 @@ public class Line_Compersion {
         System.out.println("Length of a line ("+x3+","+y3+"),"+"("+x4+","+y4+")===>"+len1);
 
         //- Using Java equals method to check equality of 2 Lengths is preferable.
-        
+
         String s1=Double.toString(len);
         String s2=Double.toString(len1);
+        System.out.println();
         if(s1.equals(s2))
         {
         	System.out.println("Two lines are equal");
         }
         else
+        {
         	System.out.println("Two lines are not equal");
+        }
         
+        //- Using Java compareTo method to compare 2 Lengths is preferable.
+        if(s1.compareTo(s2) == 0)
+        {
+        	System.out.println("Two lines are equal");
+        }
+        else if(s1.compareTo(s2) > 0)
+        {
+        	System.out.println("Length1 less then length2");
+        }
+        else if(s1.compareTo(s2) < 0)
+        {
+        	System.out.println("Length1 greater then length2");
+        }
         sc.close();
 	}
-
 }
