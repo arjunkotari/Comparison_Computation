36bc9638ab8f19df87ab2f020893f80f91d74aab
arjunkotari <86187302+arjunkotari@users.noreply.github.com>
2021-07-23 17:56:38 +0530

    Added:OOPSConcept

--- src/LineComperison/Line_Compersion.java
+++ src/LineComperison/Line_Compersion.java
@@ -3,11 +3,50 @@ package LineComperison;
 import java.util.Scanner;
 
 public class Line_Compersion {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparison Computation Program");
+	public int x1,x2,y1,y2;
+	/*
+	 * constructor to pass the values
+	 */
+	public Line_Compersion(int x1, int x2, int y1, int y2) {
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y1;
+		this.y2 = y2;
+	}
+	
+	/*
+	 * Calculating distance
+	 */
+	public double calculateDistanceOfLines() {
+		double distance = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
 		
-        double len.len1;
-        
+		return distance;
+	}
+	
+	/*
+	 * Using compareTo method to compare two lengths is preferable
+	 */
+	public static void distanceOfLine(String line1, String line2) {
+		 if(line1.compareTo(line2) == 0)
+	        {
+	        	System.out.println("Two lines are equal");
+	        }
+	        else if(line1.compareTo(line2) > 0)
+	        {
+	        	System.out.println("Length1 greter then length2");
+	        }
+	        else if(line1.compareTo(line2) < 0)
+	        {
+	        	System.out.println("Length1 less then length2");
+	        }
+	}
+	/*
+	 * main starts
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");		
+        double len1,len2;
+        //Passing values 
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter x1 point");
         int x1=sc.nextInt();        
@@ -17,48 +56,31 @@ public class Line_Compersion {
         int x2=sc.nextInt();
         System.out.println("Enter y2 point");
         int y2=sc.nextInt();
-	
-	System.out.println("Enter x3 point");
+        
+        System.out.println("Enter x3 point");
         int x3=sc.nextInt();        
         System.out.println("Enter y3 point");
         int y3=sc.nextInt();        
         System.out.println("Enter x4 point");
         int x4=sc.nextInt();
         System.out.println("Enter y4 point");
-        int y4=sc.nextInt();
-        len=Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
-        len1=Math.sqrt(Math.pow((x4-x3),2) + Math.pow((y4-y3),2));
-        System.out.println("Length of a line ("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+len);
-        System.out.println();
-        System.out.println("Length of a line ("+x3+","+y3+"),"+"("+x4+","+y4+")===>"+len1);
-
-        //- Using Java equals method to check equality of 2 Lengths is preferable.
-
-        String s1=Double.toString(len);
-        String s2=Double.toString(len1);
-        System.out.println();
-        if(s1.equals(s2))
-        {
-        	System.out.println("Two lines are equal");
-        }
-        else
-        {
-        	System.out.println("Two lines are not equal");
-        }
+        int y4=sc.nextInt();  
         
-        //- Using Java compareTo method to compare 2 Lengths is preferable.
-        if(s1.compareTo(s2) == 0)
-        {
-        	System.out.println("Two lines are equal");
-        }
-        else if(s1.compareTo(s2) > 0)
-        {
-        	System.out.println("Length1 greter then length2");
-        }
-        else if(s1.compareTo(s2) < 0)
-        {
-        	System.out.println("Length1 less then length2");
-        }
+        //calling constructor
+        Line_Compersion line = new Line_Compersion(x1, x2, y1, y2);
+        //calling calculateDistanceOfLines and store in len1.
+        len1 = line.calculateDistanceOfLines();
+        System.out.println("Length of line1: "+len1);
+        
+        Line_Compersion line1 = new Line_Compersion(x3, x4, y3, y4);
+        //calling calculateDistanceOfLines and store in len2.
+        len2 = line1.calculateDistanceOfLines();
+        System.out.println("Length of line2: "+len2);
+        //Converting double to string. Bcz compareTo method compare string type only.
+        String s1=Double.toString(len1);
+        String s2=Double.toString(len2);
+        //calling distanceOfLine method.
+        distanceOfLine(s1, s2);       
         sc.close();
 	}
 }
